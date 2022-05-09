0161f7ed1749e9b70b07eeb72580752b0dc19353
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2022-05-09 16:27:48 +0300

    Create backup1.bash

---
+++ backup1.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+backup_files="/home1 /homeuser2 /homeuser2/folder3"
+
+dest="/home/backup"
+
+dayofbackup=$(date +%A)
+username=$(hostname -s)
+archive_file="$username-$dayofbackup.tgz"
+
+
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+
+tar czf $dest/$archive_file $backup_files
+#creates an archive.
+#filter the archive through the gzip utility compressing the archive.
+#output to an archive file. Otherwise the tar output will be sent to STDOUT.
+
+echo "Backup is finished"
+date
+
+ls -lh $dest
+
+#end
